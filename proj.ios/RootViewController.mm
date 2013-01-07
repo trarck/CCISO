802d125402f930d369b7a175693f9c95121a44a8
duan.houhai <duan.houhai@denachina.com>
2013-01-07 13:18:25 +0800

    landscape support

--- proj.ios/RootViewController.mm
+++ proj.ios/RootViewController.mm
@@ -42,6 +42,17 @@
     // return UIInterfaceOrientationIsPortrait( interfaceOrientation );
 }
 
+// For ios6, use supportedInterfaceOrientations & shouldAutorotate instead
+- (NSUInteger) supportedInterfaceOrientations{
+#ifdef __IPHONE_6_0
+    return UIInterfaceOrientationMaskAllButUpsideDown;
+#endif
+}
+
+- (BOOL) shouldAutorotate {
+    return YES;
+}
+
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
